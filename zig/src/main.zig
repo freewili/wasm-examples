6fc9942195b225b33f55613fd33500d0ac2cf0bd
David Rebbe <drebbe@intrepidcs.com>
2024-08-12 16:13:19 -0400

    work in progress, still doesn't work.

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,32 +1,40 @@
 // zig build-exe -target wasm32-wasi src/main.zig -fno-entry --export=main --stack 131056 -OReleaseSmall
-const fw = @cImport({
-    @cInclude("fwwasm.h");
-});
+// const fw = @cImport({
+//     @cInclude("fwwasm.h");
+// });
+
+extern "wiliwasm" fn setled(on: c_int) void;
+extern "wiliwasm" fn waitms(ms: c_int) void;
+extern "wiliwasm" fn i2cRead(address: c_int, register: c_int, bytes: *c_char, length: c_int) c_int;
+extern "wiliwasm" fn i2cWrite(address: c_int, register: c_int, bytes: *c_char, length: c_int) c_int;
+extern "wiliwasm" fn addPanel(iPanelIndex: c_int, iVisible: c_int, iVisible: c_int, iUseTile: c_int, iTileID: c_int, iR: u8, iG: u8, iB: u8, iShowMenu: c_int) void;
+extern "wiliwasm" fn addControlPictureFromFile(iPanelIndex: c_int, iControlIndex: c_int, iX: c_int, iY: c_int, szPicture: [:0]const u8, iVisible: c_int) void;
+extern "wiliwasm" fn showPanel(iPanel: c_int) void;
 
 pub fn display_image() void {
     // Create a panel to display the image
-    fw.addPanel(0, 1, 0, 0, 0, 0, 0, 0, 1);
+    addPanel(0, 1, 0, 0, 0, 0, 0, 0, 1);
     // Add the image to the panel, this file should be in the images directory on the display processor
-    fw.addControlPictureFromFile(0, 0, 0, 0, "pip_boy.fwi", 1);
+    addControlPictureFromFile(0, 0, 0, 0, "pip_boy.fwi", 1);
     // Show the Panel
-    fw.showPanel(0);
+    showPanel(0);
 }
 
 pub export fn main() void {
-    //display_image();
+    display_image();
 
     var led_on: bool = false;
     for (0..25) |_| {
         const data_bytes: [4]u8 = .{ 0x55, 0x22, 0x11, 0x82 };
-        _ = fw.i2cWrite(
+        _ = i2cWrite(
             0x23,
             11,
             @constCast(@ptrCast(&data_bytes)),
             data_bytes.len,
         );
         // TODO: check result
-        fw.waitms(100);
-        fw.setled(@intFromBool(led_on));
+        waitms(100);
+        setled(@intFromBool(led_on));
         led_on = !led_on;
     }
 }
