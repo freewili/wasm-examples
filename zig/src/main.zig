f3c5f04e75330de8adb767b3d1677a355617d04c
David Rebbe <drebbe@intrepidcs.com>
2024-08-13 09:12:31 -0400

    picture displays now.

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,4 +1,5 @@
 // zig build-exe -target wasm32-wasi src/main.zig -fno-entry --export=main --stack 131056 -OReleaseSmall
+// This doesn't seem to work correctly. for some reason the zig compiler isn't picking up __attribute__((import_module("wiliwasm"))) __attribute__((import_name(NAME)))
 // const fw = @cImport({
 //     @cInclude("fwwasm.h");
 // });
@@ -8,14 +9,14 @@ extern "wiliwasm" fn waitms(ms: c_int) void;
 extern "wiliwasm" fn i2cRead(address: c_int, register: c_int, bytes: *c_char, length: c_int) c_int;
 extern "wiliwasm" fn i2cWrite(address: c_int, register: c_int, bytes: *c_char, length: c_int) c_int;
 extern "wiliwasm" fn addPanel(iPanelIndex: c_int, iVisible: c_int, iVisible: c_int, iUseTile: c_int, iTileID: c_int, iR: u8, iG: u8, iB: u8, iShowMenu: c_int) void;
-extern "wiliwasm" fn addControlPictureFromFile(iPanelIndex: c_int, iControlIndex: c_int, iX: c_int, iY: c_int, szPicture: [:0]const u8, iVisible: c_int) void;
+extern "wiliwasm" fn addControlPictureFromFile(iPanelIndex: c_int, iControlIndex: c_int, iX: c_int, iY: c_int, szPicture: [*c]const u8, iVisible: c_int) void;
 extern "wiliwasm" fn showPanel(iPanel: c_int) void;
 
 pub fn display_image() void {
     // Create a panel to display the image
     addPanel(0, 1, 0, 0, 0, 0, 0, 0, 1);
     // Add the image to the panel, this file should be in the images directory on the display processor
-    addControlPictureFromFile(0, 0, 0, 0, "pip_boy.fwi", 1);
+    addControlPictureFromFile(0, 0, 0, 0, @ptrCast("pip_boy.fwi"), 1);
     // Show the Panel
     showPanel(0);
 }
