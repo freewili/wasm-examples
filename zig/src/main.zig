97bf7e4e7d7edac3211d383f737b86a413f75088
David Rebbe <drebbe@intrepidcs.com>
2024-10-24 16:49:32 -0400

    updated zig example, almost working.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,56 +1,157 @@
+const std = @import("std");
+
 const fw = @cImport({
     @cInclude("fwwasm.h");
 });
 
-// different color RGB values
-const RED: i32 = 0xFF0000;
-const PINK: i32 = 0xFFC6FF;
-const ORANGE: i32 = 0xFF7F00;
-const YELLOW: i32 = 0xFFFF00;
-const GREEN: i32 = 0x00FF00;
-const LIGHT_GREEN: i32 = 0xCAFFBF;
-const BLUE: i32 = 0x0000FF;
-const LIGHT_BLUE: i32 = 0x9BF6FF;
-const INDIGO: i32 = 0x4B0082;
-const VIOLET: i32 = 0x9400D3;
+const NUMBER_OF_LEDS: i32 = 7;
 
-const Colors = [_]i32{ RED, PINK, YELLOW, GREEN, LIGHT_GREEN, BLUE, LIGHT_BLUE, INDIGO, VIOLET };
+const Color = struct {
+    red: u8,
+    green: u8,
+    blue: u8,
+};
 
-pub fn display_image() void {
+const RED = Color { .red = 0xFF, .green = 0x00, .blue = 0x00 };
+const ORANGE = Color { .red = 0xFF, .green = 0x7F, .blue = 0x00 };
+const YELLOW = Color { .red = 0xFF, .green = 0xFF, .blue = 0x00 };
+const GREEN = Color { .red = 0x00, .green = 0xFF, .blue = 0x00 };
+const LIGHT_GREEN = Color { .red = 0xCA, .green = 0xFF, .blue = 0x00 };
+const BLUE = Color { .red = 0x00, .green = 0x00, .blue = 0xBF };
+const LIGHT_BLUE = Color { .red = 0x9B, .green = 0xF6, .blue = 0xFF };
+const INDIGO = Color { .red = 0x4B, .green = 0x00, .blue = 0x82 };
+const VIOLET = Color { .red = 0x94, .green = 0x00, .blue = 0xD3 };
+const PINK = Color { .red = 0xFF, .green = 0xC6, .blue = 0xFF };
+const GRAY = Color { .red = 0x30, .green = 0x30, .blue = 0x30 };
+const WHITE = Color { .red = 0xFF, .green = 0xFF, .blue = 0xFF };
+
+const PanelInfo = struct {
+    index: u8,
+    event_type: fw.FWGuiEventType,
+    color: Color,
+    text: []const u8,
+    sub_fname: []const u8,
+};
+
+const Panels = [_]PanelInfo {
+    PanelInfo { .index = 1, .event_type = fw.FWGUI_EVENT_GRAY_BUTTON, .color = GRAY, .text = "GRAY", .sub_fname = "/radio/white.sub" },
+    PanelInfo { .index = 2, .event_type = fw.FWGUI_EVENT_YELLOW_BUTTON, .color = YELLOW, .text = "YELLOW", .sub_fname = "/radio/yellow.sub" },
+    PanelInfo { .index = 3, .event_type = fw.FWGUI_EVENT_GREEN_BUTTON, .color = GREEN, .text = "GREEN", .sub_fname = "/radio/green.sub" },
+    PanelInfo { .index = 4, .event_type = fw.FWGUI_EVENT_BLUE_BUTTON, .color = BLUE, .text = "BLUE", .sub_fname = "/radio/blue.sub" },
+    PanelInfo { .index = 5, .event_type = fw.FWGUI_EVENT_RED_BUTTON, .color = RED, .text = "RED", .sub_fname = "/radio/red.sub" },
+};
+
+const Buttons = [_]fw.FWGuiEventType {
+    fw.FWGUI_EVENT_GRAY_BUTTON,
+    fw.FWGUI_EVENT_YELLOW_BUTTON,
+    fw.FWGUI_EVENT_GREEN_BUTTON,
+    fw.FWGUI_EVENT_BLUE_BUTTON,
+    fw.FWGUI_EVENT_RED_BUTTON,
+};
+
+pub fn setup_panels() void {
     // Create a panel to display the image
     fw.addPanel(0, 1, 0, 0, 0, 0, 0, 0, 1);
     // Add the image to the panel, this file should be in the images directory on the display processor
     fw.addControlPictureFromFile(0, 0, 0, 0, @ptrCast("pip_boy.fwi"), 1);
     // Show the Panel
     fw.showPanel(0);
+
+    for (Panels) |panel| {
+        fw.addPanel(panel.index, 1, 0, 0, 0, panel.color.red, panel.color.green, panel.color.blue, 1);
+        fw.addControlText(panel.index, 1, 10, 50, 2, 0, 0, 0, 0, @ptrCast(panel.text));
+    }
 }
 
-pub export fn main() void {
-    const MAX_LOOPS: i32 = 20;
-    const NUM_LEDS: i32 = 7;
-    const DELAY_MS: i32 = 50;
-    const LED_FADE_DURATION: i32 = 300;
-
-    display_image();
-
+pub fn show_rainbow_leds(max_loops: u32) void {
+    const Colors = [_]Color {
+        RED,
+        ORANGE,
+        YELLOW,
+        GREEN,
+        LIGHT_GREEN,
+        BLUE,
+        LIGHT_BLUE,
+        INDIGO,
+        VIOLET,
+        PINK,
+        GRAY,
+        WHITE,
+    };
     var color_choice: u32 = 0;
-    var led_on: bool = false;
-    for (0..MAX_LOOPS) |_| {
-        for (0..NUM_LEDS) |led_index| {
+    for (0..max_loops) |_| {
+        for (0..NUMBER_OF_LEDS) |led_index| {
             // Pick the color and set it
-            const color: i32 = Colors[color_choice];
-            fw.setBoardLED(@intCast(led_index), (color & 0xFF0000) >> 16, (color & 0x00FF00) >> 8, (color & 0x0000FF) >> 0, LED_FADE_DURATION, fw.ledpulsefade);
+            const color: Color = Colors[color_choice];
+            fw.setBoardLED(@intCast(led_index), color.red, color.green, color.blue, 300, fw.ledpulsefade);
             // Make sure we don't overflow the array
             if (color_choice >= Colors.len - 1) {
                 color_choice = 0;
             } else {
                 color_choice += 1;
             }
-            // Lets blink the led on the PCB also
-            //fw.setled(@intFromBool(led_on));
-            led_on = !led_on;
-
-            fw.waitms(DELAY_MS);
+            fw.waitms(50);
         }
     }
+}
+
+pub fn process_events() void {
+    fw.printInt(@ptrCast("\nListening to events...\n"), fw.printColorNormal, fw.printUInt32, 0);
+    var red_count: u8 = 0;
+    while (true) {
+        fw.waitms(33);
+        if (fw.hasEvent() == 0) {
+            continue;
+        }
+        var event_data = [_]u8{0} ** fw.FW_GET_EVENT_DATA_MAX;
+        const last_event = fw.getEventData(&event_data);
+        fw.printInt(@ptrCast("\nLast event: %d\n"), fw.printColorNormal, fw.printUInt32, last_event);
+        // We only want to process button presses
+        var found: bool = false;
+        for (Buttons) |button| {
+            if (button == last_event) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            continue;
+        }
+        // if (std.binary.search(Buttons, last_event) == -1) {
+        //     continue;
+        // }
+        for (Panels) |panel| {
+            if (panel.event_type == last_event) {
+                fw.showPanel(panel.index);
+                // If we match the event type, show the panel, flash leds and transmit the radio
+                for (0..NUMBER_OF_LEDS) |led_index| {
+                    fw.setBoardLED(@intCast(led_index), panel.color.red, panel.color.green, panel.color.blue, 250, fw.ledpulse);
+                }
+                _ = fw.RadioTxSubFile(@intCast(1), @ptrCast(panel.sub_fname));
+                // show the main panel
+                fw.showPanel(0);
+                break;
+            }
+        }
+        // we need an exit condition
+        if (last_event == fw.FWGUI_EVENT_RED_BUTTON) {
+            fw.printInt("\nLast event was red button: %d\n", fw.printColorNormal, fw.printUInt32, red_count);
+            red_count += 1;
+            if (red_count >= 3) {
+                _ = fw.RadioTxSubFile(1, @ptrCast("/radio/off.sub"));
+                fw.exitToMainAppMenu();
+                break;
+            }
+        } else {
+            red_count = 0;
+        }
+    }
+}
+
+pub export fn main() void {
+    setup_panels();
+
+    show_rainbow_leds(5);
+
+    process_events();
 }
